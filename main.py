724c505bf3bded1a1fc049305635d9f15843fea1
SirSidge <heinz.hscharlach@gmail.com>
2025-01-25 13:38:23 +0200

    Added player to game

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -9,12 +10,20 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    #instantiate player
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        
+        
+        #Positioning matters. Don't draw under the 'tick'
+        #limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
